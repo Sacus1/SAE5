8cc75ae8fa5c0e27c71c174fc7702bd6d933adf3
ACBorgia <fakeilovewash@gmail.com>
2023-12-19 16:51:14 +0100

    Setup backend endpoints

--- website/backend/routes/login.js
+++ website/backend/routes/login.js
@@ -5,18 +5,18 @@ const connection = require("../database/database");
 router
   .route("/")
   .get((req, res) => {
-    console.log("Get request");
-
+    res.status(200).send("Get request received");
     // This is a prepared statement
   })
   .post((req, res) => {
-    console.log("Login post request received");
-    mail = req.mail;
-    password = req.password;
-    query = "SELECT * FROM `Client` WHERE `mail` = ? AND `password` = ?";
+    mail = req.body.mail;
+    password = req.body.password;
+    query = "SELECT * FROM `Client` WHERE `mail` = ? AND `motDePasse` = ?";
     connection.execute(query, [mail, password], (err, rows, columns) => {
       if (err) res.status(500).send(err);
-      else res.status(200).send(rows);
+      else {
+        res.status(200).send(rows);
+      }
     });
 
     connection.unprepare(query);
--- website/backend/server.js
+++ website/backend/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+
+const corsOptions = {
+  //origin: "https://localhost:5173/",
+  origin: "*",
+  method: ["POST", "GET", "PUT", "DELETE"],
+};
 const app = express();
 
+app.use(cors(corsOptions));
+app.use(express.json()); // Parse JSON-encoded bodies
+app.use(express.urlencoded({ extended: false })); // Parse URL-encoded
+
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
 
 const userRouter = require("./routes/login");
 
 app.use("/login", userRouter);
 
-app.listen(3001, () => {
-  console.log("express running on port 3001");
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
